6292c6c6b30318c8618b3441c905901d0487185b
yangming (I) <yWX1358657@china.huawei.com>
2025-01-22 10:56:36 +0800

    测试qt信号触发

---
+++ widget.h
@@ -0,0 +1,15 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+class TestWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    TestWidget();
+    ~TestWidget();
+};
+
+#endif // WIDGET_H
